c66784332c7df511cb071a98e4eedd13f4272b0e
Tobiloba <omitomo.a@gotruck.io>
2021-08-31 10:26:42 +0100

    predeployment changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 
@@ -12,7 +11,7 @@ weatherForm.addEventListener("submit", (e) => {
 
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`http://localhost:5000/weather?address=${location}`).then(
+  fetch(`/weather?address=${location}`).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
